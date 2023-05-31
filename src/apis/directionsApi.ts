5105c124febee479280445c245e54c3e3910f133
alvaroBetter <124126969+alvaroBetter@users.noreply.github.com>
2023-05-31 11:10:54 +0200

    react-mapbox project

---
+++ src/apis/directionsApi.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+
+const directionsApi = axios.create({
+	baseURL: "https://api.mapbox.com/directions/v5/mapbox/driving",
+	params: {
+		alternatives: false,
+		geometries: "geojson",
+		overview: "simplified",
+		steps: false,
+		access_token:
+			"pk.eyJ1IjoiYWx2YXJvYm0iLCJhIjoiY2xoamFmM2d4MGcydjNzcGhoODBjYmxxcSJ9.tnIB27Q55YG--JhODGNAXw",
+	},
+});
+
+
+export default directionsApi;
